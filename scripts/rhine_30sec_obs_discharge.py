a616d4dad8d00a98b7976e874167e2464ec7bb4c
Edwin Sutanudjaja <edwinkost@UU105378>
2025-03-13 15:58:34 +0100

    add initial script

--- scripts/rhine_30sec_obs_discharge.py
+++ scripts/rhine_30sec_obs_discharge.py
@@ -79,25 +79,29 @@ rootgrp.sync()
 rootgrp.close()
 
 
+# assign the time variables based on the length of grdc file
+rootgrp = nc.Dataset(ncFileName,  'a')
+datetime_base =  datetime.datetime(1990, 1, 1,0)
+datetime_last =  datetime.datetime(2010,12,31,0)
+num_of_days   = (datetime_last - datetime_base).days 
+datetime_list = [datetime_base + datetime.timedelta(days = x) for x in range(len(num_of_days))]
+print(datetime_list)
+rootgrp.variables["time"][:] = nc.date2num(datetime_list, rootgrp.variables["time"].units, rootgrp.variables["time"].calendar)
+rootgrp.sync()
+rootgrp.close()
+
+
 # assign discharge values from a station
 #
 # step 1: read GRDC nc discharge file (note the grdc discharge file must contain only the dates 1 Jan 1990 to 31 Dec 2010, e.g. using: "cdo selyear,1990/2010 input.nc output.nc")
-grdc_discharge_file = "/home/edwin/github/edwinkost/create_netcdf/example_data/basel_daily_1990-2010.nc" 
+# ~ grdc_discharge_file = "/home/edwin/github/edwinkost/create_netcdf/example_data/basel_daily_1990-2010.nc" 
+grdc_discharge_file = "/home/edwin/github/edwinkost/create_netcdf/example_data/basel_daily_1990-01.nc" 
 grdc_data        = nc.Dataset(grdc_discharge_file)
 grdc_time_series = np.array(grdc_data.variables["runoff_mean"][:])
 print(grdc_time_series)
 #
-# step 2: assign the time variables based on the length of grdc file
+# step 2: assign grdc_time_series to our netcdf file
 rootgrp = nc.Dataset(ncFileName,  'a')
-datetime_base =  datetime.datetime(1990,1,1,0)
-datetime_list = [datetime_base + datetime.timedelta(days = x) for x in range(len(grdc_time_series))]
-rootgrp.variables["time"][:] = nc.date2num(datetime_list, rootgrp.variables["time"].units, rootgrp.variables["time"].calendar)
-rootgrp.sync()
-rootgrp.close()
-#
-# step 3: assign grdc_time_series to our netcdf file
-rootgrp = nc.Dataset(ncFileName,  'a')
-# ~ rootgrp.variables[shortVarName][:,:,:] = 0.0
 # - indices for latitude and longitude
 station_latitude  = 47.5538
 station_longitude = 7.6131
@@ -107,5 +111,16 @@ minY  = min(abs(rootgrp.variables['lat'][:] - station_latitude)) # ; print(minY)
 i_lat = int(np.where(abs(rootgrp.variables['lat'][:] - station_latitude) == minY)[0])
 print(i_lat, i_lon)
 rootgrp.variables[shortVarName][:,i_lat,i_lon] = grdc_time_series
+rootgrp.sync()
+rootgrp.close()
+
+
+
+# dummy discharge values from a station
+rootgrp = nc.Dataset(ncFileName,  'a')
+i_lon = 31
+i_lat = 31
+print(i_lat, i_lon)
+rootgrp.variables[shortVarName][:,i_lat,i_lon] = grdc_time_series * 5.
 rootgrp.sync()
 rootgrp.close()
